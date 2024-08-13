b4ebd7339fe857d08b06d4b091180c5e76677210
Eugeku <eugenekuzora@gmail.com>
2024-08-13 19:40:34 +0300

    refactor: refactoring

--- src/index.js
+++ src/index.js
@@ -38,46 +38,60 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    let letters = expr.match(/.{1,10}/g);
-    console.log("letters:");
-    
-    letters = letters.map(str => str.replace(/^0*/, '')); // 10101010
-    console.log(letters);
-
-    // 10101010
-    letters = letters.map(str => {
-        let dots = str.match(/.{1,2}/g); // [10, 10, 10, 10]
-
-        console.log("dots before:");
-        console.log(dots);
-
-        // [10, 10, 10, 10]
-        dots = dots.map(element => {
-            if (element == "10") { element = "." }  // 10 
-            if (element == "11") { element = "-" }  // 11
-            console.log(element);
+    return expr.match(/.{1,10}/g).map(str => str.replace(/^0*/, '')).map(str => {
+        return str.match(/.{1,2}/g).map(element => {
+            if (element == "10") { element = "." }
+            if (element == "11") { element = "-" }
             return element;
-        });
-        console.log("dots after:");
-        console.log(dots);
-
-        dots = dots.join("");
-
-        console.log("dots after as string:");
-        console.log(dots);
-        return dots;
-    });
-
-    letters = letters.map(str => {
+        }).join("");
+    }).map(str => {
         if (str == "**********") { str = " " }
         if (str in MORSE_TABLE) { str = MORSE_TABLE[str] }
         return str;
     }).join("");
 
-    console.log("letters encoding string:");
-    console.log(letters);
-    console.log("__________________________________________");
-    return letters;
+    // OLD LOGIC leave it just to remember what is going on with logging
+
+    // let letters = expr.match(/.{1,10}/g);
+    // console.log("letters:");
+
+    // letters = letters.map(str => str.replace(/^0*/, '')); // 10101010
+    // console.log(letters);
+
+    // // 10101010
+    // letters = letters.map(str => {
+    //     let dots = str.match(/.{1,2}/g); // [10, 10, 10, 10]
+
+    //     console.log("dots before:");
+    //     console.log(dots);
+
+    //     // [10, 10, 10, 10]
+    //     dots = dots.map(element => {
+    //         if (element == "10") { element = "." }  // 10 
+    //         if (element == "11") { element = "-" }  // 11
+    //         console.log(element);
+    //         return element;
+    //     });
+    //     console.log("dots after:");
+    //     console.log(dots);
+
+    //     dots = dots.join("");
+
+    //     console.log("dots after as string:");
+    //     console.log(dots);
+    //     return dots;
+    // });
+
+    // letters = letters.map(str => {
+    //     if (str == "**********") { str = " " }
+    //     if (str in MORSE_TABLE) { str = MORSE_TABLE[str] }
+    //     return str;
+    // }).join("");
+
+    // console.log("letters encoding string:");
+    // console.log(letters);
+    // console.log("__________________________________________");
+    // return letters;
 }
 
 module.exports = {
